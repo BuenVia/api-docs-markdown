25c7a4b40e47c4c95e9d55133b246fc2908977a5
Matthew Clifford <matthewclifford@hotmail.co.u>
2023-11-13 20:05:26 +0000

    dupate

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const pg = require('pg')
 
+// Set app details
 const app = express()
-
 let port = 8000
 
-app.get('/', (req, res) => {
-    res.send('Hello world!')
+// Set DB details
+const db = new pg.Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'api_docs',
+    password: 'Guitara_11',
+    port: 5432
+})
+db.connect()
+
+// Middleware
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(`${__dirname}/public`))
+app.use(express.json())
+
+// DB Helper Functions
+async function getCategories () {
+    const results = await db.query('SELECT * FROM call_categories')
+    return results.rows
+}
+
+const getCalls = async () => {
+    const results = await db.query('SELECT * FROM calls JOIN call_categories ON calls.id = category_id')
+    return results.rows;
+}
+
+// Routing
+app.get('/', async (req, res) => {
+    const calls = await getCalls()
+    const categories = await getCategories()
+    res.render('index', { categories: categories, calls: calls })
 })
 
 
--- database.sql
+++ database.sql
@@ -23,48 +23,49 @@ VALUES (
     'Get', 
     '/v1/person/<person external_id>', 
     '# Example JSON response:
-```{"external_id": "GGREEN111",
-"user_id": 61,
-"staffno": "003",
-"username": "george.green@default.com",
-"displayname": "George Green",
-"title": "Dr",
-"firstname": "George",
-"surname": "Green",
-"badge_no": "3231384894",
-"staff_no": "003",
-"detail_version": 1,
-"preferred_name": null,
-"middle_name": null,
-"date_of_birth": "1932-12-18",
-"nationality": "Spanish",
-"national_id": "AABBCCXXXXX3",
-"passport_number": "PASS003",
-"passport_issue_date": "2011-12-18",
-"passport_expiry_date": "2024-12-18"
-}```', 1
+    ```{"external_id": "GGREEN111",
+        "user_id": 61,
+        "staffno": "003",
+        "username": "george.green@default.com",
+        "displayname": "George Green",
+        "title": "Dr",
+        "firstname": "George",
+        "surname": "Green",
+        "badge_no": "3231384894",
+        "staff_no": "003",
+        "detail_version": 1,
+        "preferred_name": null,
+        "middle_name": null,
+        "date_of_birth": "1932-12-18",
+        "nationality": "Spanish",
+        "national_id": "AABBCCXXXXX3",
+        "passport_number": "PASS003",
+        "passport_issue_date": "2011-12-18",
+        "passport_expiry_date": "2024-12-18"
+    }```', 
+1
 ), 
 (
-    'Get All Assignments'.
+    'Get All Assignments',
     'Get',
     '/v1/assignment',
     '# Query Parametersd
     ```detail=0, 1, 2 (default is 0) paging-max-results=n (default is empty, meaning unlimited) paging-offset=n (default is 0)```
     # Example JSON Response:
     ```Example JSON response:
-{
-"external_id": "98765",
-"absence_reason": "Self Certified Sick",
-"absence_notes": "Xmas 2 day break",
-"absence_duration_type": "Full Day",
-"start_date": "2017-12-20",
-"end_date": "2017-12-25",
-"assignment": "JOB3"
-}```',
+    {
+        "external_id": "98765",
+        "absence_reason": "Self Certified Sick",
+        "absence_notes": "Xmas 2 day break",
+        "absence_duration_type": "Full Day",
+        "start_date": "2017-12-20",
+        "end_date": "2017-12-25",
+        "assignment": "JOB3"
+    }```',
     2
 ), 
 (
-    'Get Absence'.
+    'Get Absence',
     'Get',
     '/v1/absence/<absence id>',
     '# Query Parameters
